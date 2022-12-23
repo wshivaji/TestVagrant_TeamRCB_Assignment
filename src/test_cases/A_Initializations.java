740779b560e9f42478ff606fbe546eb9a8063a04
wshivaji <31723435+wshivaji@users.noreply.github.com>
2022-12-24 00:06:24 +0530

    variables initialization and creation of test frame

--- src/test_cases/A_Initializations.java
+++ src/test_cases/A_Initializations.java
@@ -1,8 +1,27 @@
 package test_cases;
 
+
+import org.json.simple.JSONObject;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
 public class A_Initializations {
 
+	public static JSONObject jobj; // create public json object to fetch data from json file.
+	public static ExtentReports report; // create public report object for extent report
+	public static ExtentSparkReporter spark; // create spark object to update extent report.
 	
-	
-	
+	 
+	A_Initializations() {
+		A_Initializations.jobj = new JSONObject();
+		A_Initializations.report = new ExtentReports();
+		A_Initializations.spark = new ExtentSparkReporter("Extent_Reports\test_reports.html");
+		A_Initializations.spark.config().setDocumentTitle("TeamRCB_Assignment_Test_Report");
+		A_Initializations.spark.config().setReportName("TeamRCB JSON Data Verification & Validation Tests");
+		A_Initializations.spark.config().setTheme(Theme.DARK);
+		A_Initializations.spark.config().setEncoding("utf-8");
+		A_Initializations.report.attachReporter(A_Initializations.spark);
+	}
 }
--- src/test_cases/Smoke_Tests.java
+++ src/test_cases/Smoke_Tests.java
@@ -1,5 +1,41 @@
 package test_cases;
 
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 public class Smoke_Tests {
 
+	@BeforeTest
+	public void before_test_init() {
+		
+	}
+	
+	@Test
+	public void TC_01_verify_json_data_received_as_required() {
+		
+	}
+	
+	@Test
+	public void TC_02_verify_all_key_value_pairs_received() {
+		
+	}
+	
+	@Test
+	public void TC_03_verify_at_least_one_player_is_foraigner_in_team() {
+		
+	}
+	
+	@Test
+	public void TC_04_verify_at_least_one_wicket_keeper_available_in_team() {
+		
+	}
+	
+	
+	@AfterTest
+	public void after_test_closing() {
+		
+	}
+	
+	
 }
